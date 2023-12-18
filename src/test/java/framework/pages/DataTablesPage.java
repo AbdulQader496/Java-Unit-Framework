1b383bc351b429c0c17ba9fac1f896d5491f5065
Abdul Qader <gm.abdul.qader@gmail.com>
2023-12-18 17:19:44 +0600

    TODO task completed

--- src/test/java/framework/pages/DataTablesPage.java
+++ src/test/java/framework/pages/DataTablesPage.java
@@ -13,18 +13,34 @@ import java.util.List;
 public class DataTablesPage extends Form {
     private static final String NAME = "Data Tables";
     private final By DUE = By.xpath("//*[@id='table1']//td[4]");
+    private final By DUE_TABLE_2 = By.xpath("//*[@id='table2']//td[4]");
 
     public DataTablesPage() {
         super(By.xpath(String.format(LocatorConstants.PRECISE_TEXT_XPATH, NAME)), NAME);
     }
 
     public List<String> getFirstDueList() {
-        // todo: implement
-        return null;
+        List<String>dueList = new ArrayList<>();
+        for (ILabel label: getFirstDueLblList()) {
+            dueList.add(label.getText());
+        }
+        return dueList;
     }
 
     private List<ILabel> getFirstDueLblList() {
         return AqualityServices.getElementFactory().findElements(DUE, "due",
                 ElementType.LABEL);
+    }
+
+    public List<String> getSecondDueList() {
+        List<String> secondTableDueList = new ArrayList<>();
+        for (ILabel label: getSecondDueLblList()) {
+            secondTableDueList.add(label.getText());
+        }
+        return secondTableDueList;
+    }
+
+    private List<ILabel> getSecondDueLblList() {
+        return AqualityServices.getElementFactory().findElements(DUE_TABLE_2, "table 2 due list", ElementType.LABEL);
     }
 }
--- src/test/java/framework/tests/DataTableTests.java
+++ src/test/java/framework/tests/DataTableTests.java
@@ -11,13 +11,25 @@ public class DataTableTests extends BaseTest {
     private DataTablesPage dataTablesPage = new DataTablesPage();
 
 
+
+
     @Test
-    public void dataTableTest() {
+    public void dataFirstTableTest() {
         mainPage.clickNavigationLink(MainPageNavigation.SORTABLE_DATA_TABLES);
         Double actualSum = 0.0;
         for (String due: dataTablesPage.getFirstDueList()) {
             actualSum += StringUtils.getDoubleFromString(due);
         }
         Assert.assertEquals(actualSum, SettingsTestData.getDataTableData().getExpectedDueSum(), "Sum is not correct");
+    }
+
+    @Test
+    public void dataSecondTableTest() {
+        mainPage.clickNavigationLink(MainPageNavigation.SORTABLE_DATA_TABLES);
+        Double actualSum = 0.0;
+        for (String due: dataTablesPage.getSecondDueList()) {
+            actualSum += StringUtils.getDoubleFromString(due);
+        }
+        Assert.assertEquals(actualSum, SettingsTestData.getDataTableData().getExpectedDueSum(), "Sum of 2nd table is not correct");
     }
 }
