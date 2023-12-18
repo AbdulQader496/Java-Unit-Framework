cc133dd8b8310bbe3aa2f5df3180c0621ec150cd
Abdul Qader <gm.abdul.qader@gmail.com>
2023-12-19 01:58:26 +0600

    TODO task completed

--- src/test/java/framework/tests/FileUploadTest.java
+++ src/test/java/framework/tests/FileUploadTest.java
@@ -1,6 +1,8 @@
 package framework.tests;
 
+import framework.pages.FileUploadPage;
 import framework.pages.MainPageNavigation;
+import framework.pages.UploadedFilePage;
 import framework.utils.SettingsTestData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,12 +11,16 @@ import java.io.File;
 
 
 public class FileUploadTest extends BaseTest {
+    private FileUploadPage fileUploadPage = new FileUploadPage();
+    private UploadedFilePage uploadedFilePage = new UploadedFilePage();
     public final String FILE_NAME = SettingsTestData.getFileData().getUploadFile();
     public final String FILE_PATH = SettingsTestData.RESOURCE_FILE_PATH + FILE_NAME;
 
-
     @Test
     public void fileUploadTest() {
-        // todo: add test
+        mainPage.clickNavigationLink(MainPageNavigation.FILE_UPLOAD);
+        fileUploadPage.uploadFile(new File(FILE_PATH).getAbsolutePath());
+        fileUploadPage.clickSubmitBtn();
+        Assert.assertEquals(uploadedFilePage.getUploaadedFileName(), FILE_NAME, "File name is not correct or not uploaded");
     }
 }
