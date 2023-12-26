4441f899d28b03debe154e9cd79175cd6e5a9c01
Abdul Qader <gm.abdul.qader@gmail.com>
2023-12-27 01:24:12 +0600

    prodenv added

--- src/test/java/framework/utils/SettingsTestData.java
+++ src/test/java/framework/utils/SettingsTestData.java
@@ -15,8 +15,8 @@ public class SettingsTestData {
 
     public final String RESOURCE_FILE_PATH = PATH_OF_THE_PROJECT + "\\src\\test\\resources\\";
     private final String ENV_FILE_PATH = RESOURCE_FILE_PATH + "env.json";
-    //    private final String PROD_ENV_FILE_PATH = RESOURCE_FILE_PATH + "\\prodenv.json";
-    private final String PROD_ENV_FILE_PATH = RESOURCE_FILE_PATH + "env.json";
+    private final String PROD_ENV_FILE_PATH = RESOURCE_FILE_PATH + "\\prodenv.json";
+//    private final String PROD_ENV_FILE_PATH = RESOURCE_FILE_PATH + "env.json";
     private final String USER_FILE_PATH = RESOURCE_FILE_PATH + "userData.json";
     private final String DATA_TABLE_FILE_PATH = RESOURCE_FILE_PATH + "dataTableData.json";
     private final String FILE_DATA_PATH = RESOURCE_FILE_PATH + "fileData.json";
